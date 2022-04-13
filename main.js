a3e0dda7d8a54c2abac6f23ce10d729b54832a18
marcel.rosa <marcel.rosa@globallogic.com>
2022-04-13 16:24:31 +0200

    add abbend on click

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const map = document.querySelector(".map");
-let topY = 0;
-let leftX = 0;
-const cellSize = 30;
+const player = document.querySelector(".player");
+let topY = -window.innerHeight / 2;
+let leftX = -window.innerWidth / 2;
+const cellSize = 50;
 
 // document.addEventListener("keydown", (e) => {
 //   console.log(e);
@@ -25,27 +26,42 @@ const cellSize = 30;
 //   map.style = `top: ${topY}px; left: ${leftX}px`;
 // });
 
+map.style = `transform: translate3d(${leftX}px, ${topY}px, 0px)`;
+player.style = `transform: translate3d(${-leftX}px, ${-topY}px, 0px)`;
+
 let keysPressed = {};
 
 document.addEventListener("keydown", (e) => {
   keysPressed[e.key] = true;
 
-  if ((keysPressed["s"] && e.key == "a") || (keysPressed["a"] && e.key == "s")) {
+  if (
+    (keysPressed["s"] && e.key == "a") ||
+    (keysPressed["a"] && e.key == "s")
+  ) {
     topY -= cellSize;
     leftX += cellSize;
   }
 
-  if ((keysPressed["s"] && e.key == "d") || (keysPressed["d"] && e.key == "s")) {
+  if (
+    (keysPressed["s"] && e.key == "d") ||
+    (keysPressed["d"] && e.key == "s")
+  ) {
     topY -= cellSize;
     leftX -= cellSize;
   }
 
-  if ((keysPressed["w"] && e.key == "a") || (keysPressed["a"] && e.key == "w")) {
+  if (
+    (keysPressed["w"] && e.key == "a") ||
+    (keysPressed["a"] && e.key == "w")
+  ) {
     topY += cellSize;
     leftX += cellSize;
   }
 
-  if ((keysPressed["w"] && e.key == "d") || (keysPressed["d"] && e.key == "w")) {
+  if (
+    (keysPressed["w"] && e.key == "d") ||
+    (keysPressed["d"] && e.key == "w")
+  ) {
     topY += cellSize;
     leftX -= cellSize;
   }
@@ -66,21 +82,20 @@ document.addEventListener("keydown", (e) => {
     leftX += cellSize;
   }
 
-
-  map.style = `top: ${topY}px; left: ${leftX}px`;
+  map.style = `transform: translate3d(${leftX}px, ${topY}px, 0px)`;
+  player.style = `transform: translate3d(${-leftX}px, ${-topY}px, 0px)`;
 });
 
 document.addEventListener("keyup", (e) => {
   delete keysPressed[e.key];
 });
 
-const placeGameObject = (x, y) => {
-  const object = document.createElement("div");
-  object.style = `width: ${cellSize - 2}px; height: ${
-    cellSize - 2
-  }px; background: black; position: absolute; transform: translate(${
-    x * cellSize
-  }px, ${y * cellSize}px);`;
+const placeGameObject = (x, y, html) => {
+  const object = document.createElement('template');
+  object.innerHTML = html;
+  object.style = `width: ${cellSize}px; height: ${cellSize}px; background: black; position: absolute; transform: translate(${
+    x * cellSize - cellSize
+  }px, ${y * cellSize - cellSize}px);`;
   map.appendChild(object);
 };
 
@@ -143,4 +158,26 @@ mapGrid.forEach((item, index) => {
   setTimeout(() => {
     placeGameObject(item.x, item.y);
   }, index * 300);
+});
+
+map.addEventListener("click", (e) => {
+  if (e.target.className === "map") {
+    console.log(e.target.className);
+    let x = 0;
+    let y = 0;
+    const offsetX = e.offsetX - 7;
+    if (offsetX % cellSize < 15) {
+      x = offsetX - (offsetX % cellSize);
+    } else {
+      x = offsetX - (offsetX % cellSize) + cellSize;
+    }
+
+    if (e.offsetY % cellSize < 15) {
+      y = e.offsetY - (e.offsetY % cellSize);
+    } else {
+      y = e.offsetY - (e.offsetY % cellSize) + cellSize;
+    }
+
+    placeGameObject(x / cellSize, y / cellSize);
+  }
 });
